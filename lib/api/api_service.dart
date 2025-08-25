dc4b3f9e78a10e4bc589418f8f82216f4b42e19a
hongsuhan <hee010709@gmail.com>
2025-08-25 16:20:54 +0900

    fixed code

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,32 +1,41 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:html' as html;
-import 'dart:typed_data';
 
 class ApiService {
   Future<void> downloadCertWeb(String common_name, country_name, province_name, local_name, org_name) async {
     var url = Uri.parse('https://220.149.241.73:5000/get_key');
 
-    var request = http.MultipartRequest('POST', url);
-    request.fields['common_name'] = common_name;
-    request.fields['country_name']= country_name;
-    request.fields['province_name']= province_name;
-    request.fields['local_name']= local_name;
-    request.fields['org_name']= org_name;
+    try {
+      var response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'common_name': common_name,
+          'country_name': country_name,
+          'province_name': province_name,
+          'local_name': local_name,
+          'org_name': org_name,
+        }),
+      );
 
-    var streamedResponse = await request.send();
+      if (response.statusCode == 200) {
+        // ZIP 파일은 bodyBytes 사용
+        final bytes = response.bodyBytes;
 
-    if (streamedResponse.statusCode == 200) {
-      Uint8List bytes = await streamedResponse.stream.toBytes();
+        final blob = html.Blob([bytes], 'application/zip');
+        final downloadUrl = html.Url.createObjectUrlFromBlob(blob);
+        final anchor = html.AnchorElement(href: downloadUrl)
+          ..setAttribute('download', '$common_name.zip')
+          ..click();
+        html.Url.revokeObjectUrl(downloadUrl);
 
-      final blob = html.Blob([bytes]);
-      final downloadUrl = html.Url.createObjectUrlFromBlob(blob);
-      final anchor = html.AnchorElement(href: downloadUrl)
-        ..setAttribute('download', '$common_name.zip')
-        ..click();
-      html.Url.revokeObjectUrl(downloadUrl);
-    } else {
-      print('서버 오류: ${streamedResponse.statusCode}');
+        print('다운로드 성공');
+      } else {
+        print('서버 오류: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('예외 발생: $e');
     }
   }
-
 }
